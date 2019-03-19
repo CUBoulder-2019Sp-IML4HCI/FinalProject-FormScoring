cb10ad9b6b7a1947c6f683d44dc45fee98fc8e8b
Ian Wilkins <ian.wilkins@colorado.edu>
2019-03-19 11:21:37 -0600

    03/20/2019 Update: Updated python OSC server to handle Microbit ID (currently 'a' and 'b'), received signal strength, and xyz outputs. Updated Mircobit hex for 'a' and 'b' sattelites: both/either microbits will send an initial string message over radio when the 'A' button is pressed, this string message triggers the first onReceivedString() message which includes the ID of the Microbit ('a' or 'b'), the signal strength between 'a' and  'b', and the xyz output of the sending Microbit. Only 'a' and 'b' send radio signals in order to ensure that all signals received by 'a' (but not sent by 'a') are sent from 'b', and all signals received by 'b' (but not sent by 'b') are sent from 'a'. This makes all signal strength readings by 'a' and 'b' correspond to the signal strength between 'a' and 'b'. All Microbits ('a', 'b', and the receiver) can received radio signals, and are on the same radio group.

--- formScoreBits_to_osc.py
+++ formScoreBits_to_osc.py
@@ -4,40 +4,25 @@
  
 import serial
 from pythonosc import udp_client
-
-class Sender:
-    def __init__(self):
-        self.output = [None]*8
-        self.send = self.wait_for_fill
-    def wait_for_fill(self):
-        if None in self.output:
-            print("Waiting on full output.")
-        else:
-            self.send = self.send_signal
-    def send_signal(self):
-        print(self.output)
-        client.send_message("/final", self.output)
-
-
+ 
 # change this string depending upon where your computer makes a device for the micro:bit
-serialport = "/dev/cu.usbmodem14302"
+serialport = "/dev/ttyACM1"
  
 ser = serial.Serial(serialport, 115200)
 client = udp_client.SimpleUDPClient("localhost", 8999)
-S = Sender()
+ 
 while(True):
     line = ser.readline().decode('utf8').strip()
-
-    message = list(map(float, line.split("^")))
-    tag = message[0]
-    if tag == "a":
-        S.output[0:3] = message[0:3]
-    elif tag == "b":
-        S.output[4:7] = message[0:3]
-
+    print(line)
+    
+    if line.count('^') != 4: continue
  
-    S.send()
-
+    #n, s, x, y, z = map(str, line.split("^"))
+    n, s, x, y, z = line.split("^")
+ 
+    print(f"({n}, {s}, {x}, {y}, {z})")
+ 
+    client.send_message("/form_data", [n, s, x, y, z] )
 
 #import serial
 #from pythonosc import udp_client
