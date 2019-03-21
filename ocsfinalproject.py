be7e2e0dbade73e4bc2856720184f3cb6e4cb0ab
GarrettSippel <gasi9744@colorado.edu>
2019-03-21 12:02:28 -0600

    osc handling

--- ocsfinalproject.py
+++ ocsfinalproject.py
@@ -1,29 +1,47 @@
 # requires the following python libraries:
 #  python-osc
 #  pyserial
- 
+
+import os
 import serial
 from pythonosc import udp_client
-import os
- 
+
+class Sender:
+    def __init__(self):
+        self.output = [None]*8
+        self.send = self.wait_for_fill
+    def wait_for_fill(self):
+        if None in self.output:
+            print("Waiting on full output.")
+        else:
+            self.send = self.send_signal
+    def send_signal(self):
+        print(self.output)
+        client.send_message("/final", self.output)
+
+
 # change this string depending upon where your computer makes a device for the micro:bit
-serialport = "/dev/ttyACM1"
+if os.name == 'nt':
+    serialport = "COM4"
+else:
+    serialport = "/dev/cu.usbmodem14302"
+
  
 ser = serial.Serial(serialport, 115200)
 client = udp_client.SimpleUDPClient("localhost", 8999)
- 
+S = Sender()
 while(True):
     line = ser.readline().decode('utf8').strip()
-    print(line)
-    
-    if line.count('^') != 4: continue
- 
-    #n, s, x, y, z = map(str, line.split("^"))
-    n, s, x, y, z = line.split("^")
- 
-    print(f"({n}, {s}, {x}, {y}, {z})")
- 
-    client.send_message("/form_data", [n, s, x, y, z] )
+
+    message = list(map(float, line.split("^")))
+    tag = message[0]
+    if tag == "a":
+        S.output[0:3] = message[1:3]
+    elif tag == "b":
+        S.output[4:7] = message[1:3]
+    S.send()
+    print(S.output)
+
 
 #import serial
 #from pythonosc import udp_client
@@ -43,4 +61,3 @@ while(True):
 #        
 #    if(len(data) == 6):        
 #        print(f"({data[0]}, {data[1]}, {data[2]}, {data[3]}, {data[4]}, {data[5]})")
-#        client.send_message("/wand_accel", data )
